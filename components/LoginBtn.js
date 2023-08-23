b62c4091a603e0ecc62358a008ff8e8962b85be5
simjieun <simjiuen@gmail.com>
2023-08-23 19:12:31 +0900

    firebase 연동

--- components/LoginBtn.js
+++ components/LoginBtn.js
@@ -1,11 +1,17 @@
 "use client";
 import { signIn, signOut } from "next-auth/react";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { auth } from "@/app/firebase";
 
 export default function LoginBtn() {
+  const googleSignIn = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider);
+  };
   return (
     <>
       <button onClick={() => signIn("github")}>Github Login</button>
-      <button onClick={() => signIn("google")}>Google Login</button>
+      <button onClick={() => googleSignIn()}>Google Login</button>
       <button onClick={() => signOut()}>Sign out</button>
     </>
   );
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,9 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import { FirebaseAdapter } from "@next-auth/firebase-adapter";
+import { firestore } from "@/app/firebase";
+
+
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
@@ -7,8 +11,8 @@ export const authOptions = {
       clientId: "5ec7e314b88304a1d5ed",
       clientSecret: "8014f9771ffdbf02e3b644b37303d6892dfa502a",
     }),
-    // ...add more providers here
   ],
+  adapter: FirebaseAdapter(firestore),
   secret: "qwer1234", // Add a JWT secret token (can be a random string)
 };
 export default NextAuth(authOptions);
